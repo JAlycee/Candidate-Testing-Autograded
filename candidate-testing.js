13c377885560093101de2cce414dd63d077b2c06
JAlycee <124473879+JAlycee@users.noreply.github.com>
2023-09-27 21:56:33 -0500

    update

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,54 +1,52 @@
 const input = require('readline-sync');
-let info = input.question("What is your name? ");
+
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
 let candidateName = " ";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question = "Who was the first American woman in space? ";
+let question = ("Who was the first American woman in space? ");
 let correctAnswer = "Sally Ride";
 let candidateAnswer = " ";
 
 
 //TODO: Variables for Part 2
 let questions = [
-  "Who was the first American woman in space?",
-  "True or false: 5 kilometers == 5000 meters?",
-  "(5+3)/2 * 10 = ?",
-  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?",
-  "What is the minimum crew size for the ISS?"
-];
+  "Who was the first American woman in space? " ,
+  "True or false : 5 kilometers == 5000 meters? " , 
+  "(5 + 3)/2 * 10 = ? " , 
+  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
+  "What is the minimum crew size for the ISS? "];
 
 let correctAnswers = [
-  "Sally Ride",
-  "true",
-  "40",
-  "Trajectory",
-  "3"
+"Sally Ride" ,
+"true" ,
+"40",
+"Trajectory" ,
+"3" 
 ];
-let candidateAnswers =[ ];
+let candidateAnswers =[];
 
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-input.question("Please enter your name: ");
+candidateName = input.question("Please enter your name: ");
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-input.question(question);
-
+for (let i = 0; i < questions.length; i++) {
+  let answer = input.question(`${i+1} ${questions[i]}`)
+    candidateAnswers.push(answer);
+  }
 }
-
 function gradeQuiz(candidateAnswers) {
-
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-if (candidateAnswer === correctAnswer) {
-    console.log("Congragulations! You answered correctly. ");
+  if (candidateAnswer === correctAnswer) {
+    console.log(" Congrats! You answered correctly. ");
   } else {
-    console.log("Sorry, that's not a valid answer. ")
+    console.log("Sorry, that's not a valid answer.");
     }
-
 
 let grade;  //TODO 3.2 use this variable to calculate the candidates score.
 
@@ -59,7 +57,7 @@ let grade;  //TODO 3.2 use this variable to calculate the candidates score.
 function runProgram() {
   askForName();
   // TODO 1.1c: Greet candidate using their name //
-  console.log("Hello, " + candidateName);
+  console.log(`Hello ${candidateName}`);
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
